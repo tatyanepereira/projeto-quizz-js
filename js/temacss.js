c7306395e3a4d02cf35f9b0bd7671f92d81374e4
Perkles <tatiane.pereira@outlook.com>
2023-07-29 13:18:23 -0300

    Atualização do código

---
+++ js/temacss.js
@@ -0,0 +1,104 @@
+export const temacss = [
+	{
+        title: "Qual é o seletor CSS que aplica estilo a todos os elementos HTML?",
+        alternativa: [    
+            "*",
+            "#",
+            ".",
+            "&",
+        ],
+        answer: 0,
+    },
+    {
+        title: "Qual propriedade CSS é usada para alterar o tamanho do texto?",
+        alternativa: [
+            "font-size",
+            "text-style",
+            "font-style",
+            "size-text",
+        ],
+        answer: 0,
+    },
+    {
+        title: "Como definir a cor de fundo de um elemento em CSS?",
+        alternativa: [
+            "background-color",
+            "color",
+            "bg-color",
+            "background",
+        ],
+        answer: 0,
+    },
+    {
+        title: "Qual é a propriedade CSS usada para adicionar uma sombra a um elemento?",
+        alternativa: [
+            "shadow",
+            "text-shadow",
+            "box-shadow",
+            "element-shadow",
+        ],
+        answer: 2,
+    },
+    {
+        title: "Como centralizar um elemento horizontalmente em CSS?",
+        alternativa: [
+            "align: center;",
+            "margin: auto;",
+            "float: center;",
+            "position: center;",
+        ],
+        answer: 1,
+    },
+    {
+        title: "Qual é o seletor CSS usado para aplicar estilo a um elemento quando o mouse está sobre ele?",
+        alternativa: [
+            ":mouse-over",
+            ":hover",
+            ":on-hover",
+            ":mouseover",
+        ],
+        answer: 1,
+    },
+    {
+        title: "Para arredondar as bordas de um elemento em CSS, qual propriedade você usaria?",
+        alternativa: [   
+            "border-radius",
+            "corner-radius",
+            "border-style",
+            "edge-radius",
+        ],
+        answer: 0,
+    },
+    {
+        title: "Como ocultar um elemento em CSS sem afetar o layout da página?",
+        alternativa: [
+            "display: hidden;",
+            "visibility: hidden;",
+            "display: none;",
+            "visible: false;",
+        ],
+        answer: 2,
+    },
+    {
+        title: "Qual é a propriedade CSS usada para alterar o espaçamento entre as linhas de texto?",
+        alternativa: [
+            "line-height",
+            "spacing-line",
+            "text-spacing",
+            "line-spacing",
+        ],
+        answer: 2,
+    },
+    {
+        title: "Como aplicar um estilo CSS que afeta apenas elementos pares de uma lista não ordenada (ul)?",
+        alternativa: [
+            "li:even",
+            "ul:nth-child(even)",
+            "ul:even-child",
+            "li:nth-child(even)",
+        ],
+        answer: 3,
+    }
+];
+
+
---
+++ js/temahtml.js
@@ -0,0 +1,102 @@
+export const temahtml = [
+	{
+		title: "Qual elemento HTML é usado para criar um hyperlink?",
+		alternativa: [
+			"link",
+			"a",
+			"href",
+			"hyper",
+		],
+		answer: 1,
+	},
+    {
+        title: "Qual atributo HTML é usado para definir o texto alternativo para uma imagem?",
+        alternativa: [
+            "asrc",
+            "alt",
+            "title",
+            "img",
+        ],
+        answer: 1,
+    },
+    {
+        title: "Qual tag HTML é usada para criar uma lista não ordenada?",
+        alternativa: [
+            "ul",
+            "li",
+            "ol",
+            "list",
+        ],
+        answer: 0,
+    },
+    {
+        title: "Qual elemento HTML é usado para criar um cabeçalho de nível 1?",
+        alternativa: [
+            "h1",
+            "header",
+            "head",
+            "heading",
+            ],
+        answer: 0,
+    },
+    {
+        title: "Qual atributo HTML é usado para definir o estilo em linha de um elemento?",
+        alternativa: [
+            "style",
+            "inline",
+            "css",
+            "script",
+        ],
+        answer: 0,
+    },
+    {
+        title: "Qual tag HTML é usada para criar uma tabela?",
+        alternativa: [
+            "table",
+            "tab",
+            "tbl",
+            "tr",
+        ],
+        answer: 0,
+    },
+    {
+        title: "Qual elemento HTML é usado para criar um parágrafo?",
+        alternativa: [
+            "paragraph",
+            "p",
+            "para",
+            "text",
+        ],
+        answer: 1,
+    },
+    {
+        title: "Qual atributo HTML é usado para definir uma classe que pode ser referenciada por CSS?",
+        alternativa: [
+            "class",
+            "css",
+            "classname",
+            "cl",
+        ],
+        answer: 0,
+    },
+    {
+        title: "Qual tag HTML é usada para criar um botão clicável?",
+        alternativa: [
+            "button",
+            "input",
+            "submit",
+            "click",
+        ],
+        answer: 0,
+    },
+    {
+        title: "Qual elemento HTML é usado para inserir um espaço horizontal?",
+        alternativa: [
+            "space",
+            "br",
+            "hr",
+            "newline",
+        ],
+        answer: 2,
+    }
+];
